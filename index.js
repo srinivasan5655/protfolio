190cc04c79cde9d40e396b2126155d3f0f6512ac
srinivasan5655 <srinivasan5655@gmail.com>
2021-07-21 12:40:03 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import Typed from "typed.js";
+import VanillaTilt from "vanilla-tilt";
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+const options = {
+  strings: ["<i>Front-End Developer</i> ."],
+  typeSpeed: 40,
+};
+
+var typed = new Typed(".typo", options);
+
+VanillaTilt.init(document.querySelectorAll(".button"), {
+  max: 35,
+  speed: 400,
+  scale: 1.2,
+  glare: true,
+  "max-glare": 0.5,
+});
+
+//It also supports NodeList
+VanillaTilt.init(document.querySelectorAll(".button"));
+
+const test = document.getElementById("message").focus();
